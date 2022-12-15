cd5323eca6c3be710e4b25444821653eba171bc4
Alexis Guzman <codingcodax@gmail.com>
2022-12-15 17:52:23 -0600

    chore(prettier): add prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,8 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  jsxSingleQuote: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
 };
