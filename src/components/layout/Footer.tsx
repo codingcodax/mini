c99ad3bd2e676caf7f43a24e15b07b7cd1f56b28
Alexis Guzman <codingcodax@gmail.com>
2022-12-15 21:15:51 -0600

    feat(layout): add white and black hearts for light and dark themes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,8 @@ const Footer = () => {
   return (
     <footer className='px-4 pb-5 sm:px-6 md:px-8 lg:pb-8'>
       <p className='text-center'>
-        Handcrafted with <span className='font-mono'>🤍</span> by @codingcodax
+        Handcrafted with <span className='font-mono dark:hidden'>🖤</span>{' '}
+        <span className='hidden font-mono dark:inline'>🤍</span> by @codingcodax
         ^_^
       </p>
     </footer>
