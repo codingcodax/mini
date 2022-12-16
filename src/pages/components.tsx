b5b6bc487d9925f59e91ea762e042d06c8be807e
Alexis Guzman <codingcodax@gmail.com>
2022-12-16 13:49:58 -0600

    feat(components): create components page

---
+++ src/pages/components.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Components: NextPage = () => {
+  return (
+    <div>
+      <p>Components page</p>
+    </div>
+  );
+};
+
+export default Components;
