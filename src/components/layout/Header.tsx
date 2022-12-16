0187a0a13cbaa82e2ecfd645347021095fa66b9d
Alexis Guzman <codingcodax@gmail.com>
2022-12-15 21:08:20 -0600

    feat(layout): setup header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,21 @@
+import { ExternalLink } from '~/components/atoms';
+
 const Header = () => {
   return (
-    <div>
-      <p>Header component</p>
-    </div>
+    <header className='flex items-center justify-between px-4 pt-5 sm:px-6 md:px-8 lg:pt-8'>
+      <h2 className='text-xl font-medium'>mini</h2>
+
+      <ul>
+        <li>
+          <ExternalLink
+            className='text-sm font-medium text-slate-700 hover:text-sky-500 dark:text-slate-200 dark:hover:text-sky-400'
+            href='https://github.com/codingcodax'
+          >
+            GitHub
+          </ExternalLink>
+        </li>
+      </ul>
+    </header>
   );
 };
 
