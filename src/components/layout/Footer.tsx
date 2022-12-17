ea478d625a16654f6b4370e38de0ad2e42bad6cd
Alexis Guzman <codingcodax@gmail.com>
2022-12-16 21:29:04 -0600

    feat(layout): add divider to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,7 @@
 const Footer = () => {
   return (
-    <footer className='px-4 pb-5 sm:px-6 md:px-8 lg:pb-8'>
+    <footer className='mt-16 px-4 pb-5 sm:px-6 md:px-8 lg:pb-8'>
+      <div className='mb-10 h-px w-full bg-slate-300 dark:bg-slate-800' />
       <p className='text-center'>
         Handcrafted with <span className='font-mono dark:hidden'>🖤</span>{' '}
         <span className='hidden font-mono dark:inline'>🤍</span> by @codingcodax
