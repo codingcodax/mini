e3a5f8b268e080f1bce3f4828f3504c27c73b8c2
Alexis Guzman <codingcodax@gmail.com>
2022-12-17 16:39:58 -0600

    feat(icons): create play

---
+++ src/components/atoms/icons/Play.tsx
@@ -0,0 +1,20 @@
+import type { Icon } from '~/types/icon';
+
+const Play = ({ className }: Icon) => {
+  return (
+    <svg
+      className={className}
+      fill='currentColor'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        clipRule='evenodd'
+        d='M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z'
+        fillRule='evenodd'
+      />
+    </svg>
+  );
+};
+
+export default Play;
