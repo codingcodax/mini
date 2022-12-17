a75f4b2e4a26ea750dadd0638abaf6e81ac9e611
Alexis Guzman <codingcodax@gmail.com>
2022-12-16 20:29:01 -0600

    feat(snippets): create link underline left right

---
+++ src/data/snippets/LinkUnderlineLeftRight.tsx
@@ -0,0 +1,19 @@
+import type { Snippet } from '~/types/snippet';
+
+const LinkUnderlineLeftRight: Snippet = {
+  title: 'Link Underline Left Right',
+  type: 'link',
+  githubLink:
+    'https://github.com/codingcodax/mini/tree/main/src/data/snippets/LinkUnderlineLeftRight.tsx',
+  playgroundLink: 'https://play.tailwindcss.com/dGs22vWHdK',
+  source: (
+    <a
+      className='relative inline-block text-slate-50 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-0 after:bg-gray-200 after:transition-transform after:duration-300 after:ease-in-out hover:after:origin-bottom-right hover:after:scale-x-100'
+      href='#'
+    >
+      Hover over me
+    </a>
+  ),
+};
+
+export default LinkUnderlineLeftRight;
