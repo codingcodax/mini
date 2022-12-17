717de2052736dcc6c0c5881ae998f99be377cf89
Alexis Guzman <codingcodax@gmail.com>
2022-12-17 16:49:09 -0600

    feat(icons): add github

--- src/components/atoms/icons/index.ts
+++ src/components/atoms/icons/index.ts
@@ -1,11 +1,13 @@
 import CodeBracket from './CodeBracket';
 import EllipsisHorizontal from './EllipsisHorizontal';
+import Github from './Github';
 import Play from './Play';
 import VideoCamera from './VideoCamera';
 
 const Icons = {
   CodeBracket,
   EllipsisHorizontal,
+  Github,
   Play,
   VideoCamera,
 };
