534a920f31273baa951ad48f6eefa0e1ef06a983
Alexis Guzman <codingcodax@gmail.com>
2022-12-16 20:19:26 -0600

    feat(snippets): update playground link and source

--- src/data/snippets/LinkUnderlineLeftLeft.tsx
+++ src/data/snippets/LinkUnderlineLeftLeft.tsx
@@ -5,11 +5,14 @@ const LinkUnderlineLeftLeft: Snippet = {
   type: 'link',
   githubLink:
     'https://github.com/codingcodax/mini/tree/main/src/data/snippets/LinkUnderlineLeftLeft.tsx',
-  playgroundLink: 'https://play.tailwindcss.com/wQBjrZER0p',
+  playgroundLink: 'https://play.tailwindcss.com/1A0yYsJIQs',
   source: (
-    <div className='relative inline-block cursor-pointer text-slate-50 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-0 after:bg-gray-200 after:transition-transform after:duration-300 after:ease-in-out hover:after:origin-bottom-left hover:after:scale-x-100'>
+    <a
+      className='relative inline-block text-slate-50 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-0 after:bg-gray-200 after:transition-transform after:duration-300 after:ease-in-out hover:after:origin-bottom-left hover:after:scale-x-100'
+      href='#'
+    >
       Hover over me
-    </div>
+    </a>
   ),
 };
 
