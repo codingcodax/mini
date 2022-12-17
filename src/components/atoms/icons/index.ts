885b29b6609d6cabcdb7345aa1e26083cdae693a
Alexis Guzman <codingcodax@gmail.com>
2022-12-17 16:44:02 -0600

    feat(icons): export video camera

--- src/components/atoms/icons/index.ts
+++ src/components/atoms/icons/index.ts
@@ -1,9 +1,11 @@
 import CodeBracket from './CodeBracket';
 import Play from './Play';
+import VideoCamera from './VideoCamera';
 
 const Icons = {
   CodeBracket,
   Play,
+  VideoCamera,
 };
 
 export default Icons;
