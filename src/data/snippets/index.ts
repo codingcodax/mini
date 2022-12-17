d4a48efd5c71c79b6928a6dff3141bbed1b860ee
Alexis Guzman <codingcodax@gmail.com>
2022-12-16 21:11:30 -0600

    feat(snippets): export snippets

--- src/data/snippets/index.ts
+++ src/data/snippets/index.ts
@@ -1,1 +1,17 @@
-export { default as LinkUnderlineLeftLeft } from './LinkUnderlineLeftLeft';
+import type { Snippet } from '~/types/snippet';
+
+import LinkUnderlineCenterOutside from './LinkUnderlineCenterOutside';
+import LinkUnderlineLeftLeft from './LinkUnderlineLeftLeft';
+import LinkUnderlineRightRight from './LinkUnderlineRightRight';
+import LinkUnderlineLeftRight from './LinkUnderlineLeftRight';
+import LinkUnderlineRightLeft from './LinkUnderlineRightLeft';
+
+const Snippets: Snippet[] = [
+  LinkUnderlineCenterOutside,
+  LinkUnderlineLeftLeft,
+  LinkUnderlineRightRight,
+  LinkUnderlineLeftRight,
+  LinkUnderlineRightLeft,
+];
+
+export default Snippets;
