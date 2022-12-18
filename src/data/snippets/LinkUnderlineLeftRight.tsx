1451d02cdca7176e897a864651253853987f0869
Alexis Guzman <codingcodax@gmail.com>
2022-12-17 21:39:23 -0600

    feat(snippets): update playground link to left to right

--- src/data/snippets/LinkUnderlineLeftRight.tsx
+++ src/data/snippets/LinkUnderlineLeftRight.tsx
@@ -5,13 +5,13 @@ const LinkUnderlineLeftRight: Snippet = {
   type: 'link',
   githubLink:
     'https://github.com/codingcodax/mini/tree/main/src/data/snippets/LinkUnderlineLeftRight.tsx',
-  playgroundLink: 'https://play.tailwindcss.com/dGs22vWHdK',
+  playgroundLink: 'https://play.tailwindcss.com/lTPdvpkpCX',
   source: (
     <a
       className='relative inline-block text-slate-50 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-0 after:bg-gray-200 after:transition-transform after:duration-300 after:ease-in-out hover:after:origin-bottom-right hover:after:scale-x-100'
       href='#'
     >
-      Hover over me
+      Left to right
     </a>
   ),
 };
