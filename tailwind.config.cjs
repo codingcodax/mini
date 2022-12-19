91d84aa734fc8f512613e765b0c04bf6aaed7de4
Alexis Guzman <codingcodax@gmail.com>
2022-12-19 12:29:06 -0600

    chore(tailwind): add sing keyframe and animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,20 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        swing: {
+          '15%': { transform: 'translateX(5px)' },
+          '30%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(3px)' },
+          '80%': { transform: 'translateX(2px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        swing: 'swing 1s ease 1',
+      },
+    },
   },
   plugins: [require('@headlessui/tailwindcss')],
 };
