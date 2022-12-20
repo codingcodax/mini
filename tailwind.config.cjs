6106975e49a41cc1433f4271b06351c93dea9120
Alexis Guzman <codingcodax@gmail.com>
2022-12-20 10:13:17 -0600

    chore(tailwind): add pulsar keyframe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -32,6 +32,11 @@ module.exports = {
           '50%': { transform: 'scale(1.5)' },
           '100%': { transform: 'scale(0)' },
         },
+        pulsar: {
+          '0%': { transform: 'scale(0)', opacity: '1' },
+          '50%': { transform: 'scale(1)', opacity: '0.25' },
+          '100%': { transform: 'scale(0)', opacity: '1' },
+        },
       },
       animation: {
         swing: 'swing 1s ease 1',
