bfe4fd6330b1a3a0577155ef455d3c872df8b186
Alexis Guzman <codingcodax@gmail.com>
2022-12-20 14:03:12 -0600

    feat(atoms): add link icon

---
+++ src/components/atoms/icons/Link.tsx
@@ -0,0 +1,24 @@
+import type { Icon } from '~/types/icon';
+
+const Link = ({ className }: Icon) => {
+  return (
+    <svg
+      className={className}
+      fill='none'
+      height='24'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      viewBox='0 0 24 24'
+      width='24'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M0 0h24v24H0z' fill='none' stroke='none'></path>
+      <path d='M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5'></path>
+      <path d='M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5'></path>
+    </svg>
+  );
+};
+
+export default Link;
