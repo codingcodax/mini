09434445c5cfa66557dc665e038e116e5df6ae4e
Alexis Guzman <codingcodax@gmail.com>
2022-12-20 14:05:39 -0600

    feat(atoms): export loader icon

--- src/components/atoms/icons/index.ts
+++ src/components/atoms/icons/index.ts
@@ -2,6 +2,7 @@ import CodeBracket from './CodeBracket';
 import EllipsisHorizontal from './EllipsisHorizontal';
 import Github from './Github';
 import Link from './Link';
+import Loader from './Loader';
 import Play from './Play';
 import VideoCamera from './VideoCamera';
 
@@ -10,6 +11,7 @@ const Icons = {
   EllipsisHorizontal,
   Github,
   Link,
+  Loader,
   Play,
   VideoCamera,
 };
