5120ac908ed7fd53f1d41bbe008d0cd9e247bd42
Alexis Guzman <codingcodax@gmail.com>
2022-12-21 20:58:26 -0600

    feat(components): setup select type component

--- src/components/pages/components/SelectType.tsx
+++ src/components/pages/components/SelectType.tsx
@@ -1,8 +1,36 @@
-const SelectType = () => {
+import { RadioGroup } from '@headlessui/react';
+
+import SnippetTypes from '~/data/snippetTypes';
+import type { SnippetType } from '~/types/snippet';
+
+interface SelectTypeProps {
+  selected: SnippetType | undefined;
+  setSelected: React.Dispatch<React.SetStateAction<SnippetType | undefined>>;
+}
+
+const SelectType = ({ selected, setSelected }: SelectTypeProps) => {
   return (
-    <div>
-      <p>Select Type component</p>
-    </div>
+    <RadioGroup
+      as='nav'
+      className='mx-auto mt-16 w-[calc(100vw_-_2rem)] max-w-screen-lg overflow-auto'
+      value={selected}
+      onChange={setSelected}
+    >
+      <RadioGroup.Label className='sr-only'>Snippets type</RadioGroup.Label>
+      <ul className='grid grid-cols-[repeat(6,_4rem)] gap-10 sm:w-fit'>
+        {SnippetTypes.map((snippetType) => (
+          <RadioGroup.Option key={snippetType.name} as='li' value={snippetType}>
+            <button
+              className='flex w-full min-w-[4rem] flex-col items-center text-sm font-semibold text-slate-300 ui-checked:text-sky-500 ui-not-checked:hover:text-slate-400 dark:text-slate-600 dark:ui-checked:text-sky-500 dark:ui-not-checked:hover:text-slate-500'
+              type='button'
+            >
+              {snippetType.icon}
+              {snippetType.name}
+            </button>
+          </RadioGroup.Option>
+        ))}
+      </ul>
+    </RadioGroup>
   );
 };
 
