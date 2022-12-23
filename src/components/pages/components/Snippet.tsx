0f0d48451b842f95708e693fe57ac6b6439bf711
Alexis Guzman <codingcodax@gmail.com>
2022-12-22 20:11:21 -0600

    fix(components): move dropdown out of backdrop

--- src/components/pages/components/Snippet.tsx
+++ src/components/pages/components/Snippet.tsx
@@ -23,57 +23,59 @@ const Snippet = ({
   };
 
   return (
-    <li className='relative rounded-lg bg-slate-800 backdrop-blur dark:bg-slate-900/70 dark:ring-1 dark:ring-inset dark:ring-white/10'>
-      <div className='flex h-10 items-center justify-between border-b border-slate-500/30 px-3'>
-        <div className='flex space-x-1.5'>
-          <div className='h-2 w-2 rounded-full bg-slate-600'></div>
-          <div className='h-2 w-2 rounded-full bg-slate-600'></div>
-          <div className='h-2 w-2 rounded-full bg-slate-600'></div>
+    <li className='relative'>
+      <div className='relative rounded-lg bg-slate-800 backdrop-blur dark:bg-slate-900/70 dark:ring-1 dark:ring-inset dark:ring-white/10'>
+        <div className='flex h-10 items-center border-b border-slate-500/30 px-3'>
+          <div className='flex space-x-1.5'>
+            <div className='h-2 w-2 rounded-full bg-slate-600'></div>
+            <div className='h-2 w-2 rounded-full bg-slate-600'></div>
+            <div className='h-2 w-2 rounded-full bg-slate-600'></div>
+          </div>
         </div>
 
-        <Menu as='div' className='relative h-6'>
-          <Menu.Button className='rounded-lg hover:bg-slate-700/80 dark:hover:bg-slate-800'>
-            <Icons.ChevronDown className='h-6 w-6 stroke-slate-600' />
-          </Menu.Button>
-
-          <Menu.Items className='absolute right-0 z-40 mt-2 grid w-56 rounded-md bg-slate-800 p-1 shadow-lg backdrop-blur focus:outline-none dark:bg-slate-900 dark:ring-1 dark:ring-inset dark:ring-white/10'>
-            <Menu.Item as='button' className='menu-item' onClick={copySource}>
-              <Icons.CodeBracket className='menu-icon' /> Copy Source
-            </Menu.Item>
-            <Menu.Item
-              as='button'
-              className='menu-item ui-disabled:cursor-not-allowed ui-disabled:opacity-40'
-              disabled={animation ? false : true}
-              onClick={copyAnimation}
-            >
-              <Icons.VideoCamera className='menu-icon' />
-              Copy Keyframes
-            </Menu.Item>
-            <div className='my-1 h-px w-full bg-slate-800' />
-            <Menu.Item
-              as='a'
-              className='menu-item'
-              href={playgroundLink}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <Icons.Play className='menu-icon' /> Open in Playground
-            </Menu.Item>
-            <Menu.Item
-              as='a'
-              className='menu-item'
-              href={githubLink}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <Icons.Github className='menu-icon' />
-              Open in GitHub
-            </Menu.Item>
-          </Menu.Items>
-        </Menu>
+        <div className='flex h-28 items-center justify-center'>{source}</div>
       </div>
 
-      <div className='flex h-28 items-center justify-center'>{source}</div>
+      <Menu as='div' className='absolute top-2 right-3 h-6'>
+        <Menu.Button className='rounded-lg hover:bg-slate-700/80 dark:hover:bg-slate-800'>
+          <Icons.ChevronDown className='h-6 w-6 stroke-slate-600' />
+        </Menu.Button>
+
+        <Menu.Items className='absolute right-0 z-40 mt-2 grid w-56 rounded-md bg-slate-800 p-1 shadow-lg backdrop-blur focus:outline-none dark:bg-slate-900 dark:ring-1 dark:ring-inset dark:ring-white/10'>
+          <Menu.Item as='button' className='menu-item' onClick={copySource}>
+            <Icons.CodeBracket className='menu-icon' /> Copy Source
+          </Menu.Item>
+          <Menu.Item
+            as='button'
+            className='menu-item ui-disabled:cursor-not-allowed ui-disabled:opacity-40'
+            disabled={animation ? false : true}
+            onClick={copyAnimation}
+          >
+            <Icons.VideoCamera className='menu-icon' />
+            Copy Keyframes
+          </Menu.Item>
+          <div className='my-1 h-px w-full bg-slate-800' />
+          <Menu.Item
+            as='a'
+            className='menu-item'
+            href={playgroundLink}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <Icons.Play className='menu-icon' /> Open in Playground
+          </Menu.Item>
+          <Menu.Item
+            as='a'
+            className='menu-item'
+            href={githubLink}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <Icons.Github className='menu-icon' />
+            Open in GitHub
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
     </li>
   );
 };
