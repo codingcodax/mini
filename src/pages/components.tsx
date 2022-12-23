ad49078b1211d5a5fae3876e1989c753723467d7
Alexis Guzman <codingcodax@gmail.com>
2022-12-22 21:01:41 -0600

    feat(components): add seo

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -4,6 +4,7 @@ import type { NextPage } from 'next';
 import { Steps, Snippet, SelectType } from '~/components/pages/components';
 import Snippets from '~/data/snippets';
 import SnippetTypes from '~/data/snippetTypes';
+import { Seo } from '~/components/atoms';
 
 const Components: NextPage = () => {
   const [typeSelected, setTypeSelected] = useState(SnippetTypes[0]);
@@ -14,26 +15,30 @@ const Components: NextPage = () => {
   }, [typeSelected]);
 
   return (
-    <div className='mx-auto mt-10 px-4 sm:px-6 md:px-8'>
-      <main className='max-w-6xl'>
-        <h1 className='grid text-center text-3xl font-semibold sm:text-4xl'>
-          <span>Beautiful micro components</span>
-          <span>crafted with Tailwind CSS.</span>
-        </h1>
+    <>
+      <Seo name='Components' path='/components' />
 
-        <Steps />
-      </main>
+      <div className='mx-auto mt-10 px-4 sm:px-6 md:px-8'>
+        <main className='max-w-6xl'>
+          <h1 className='grid text-center text-3xl font-semibold sm:text-4xl'>
+            <span>Beautiful micro components</span>
+            <span>crafted with Tailwind CSS.</span>
+          </h1>
 
-      <SelectType selected={typeSelected} setSelected={setTypeSelected} />
+          <Steps />
+        </main>
 
-      <section className='mx-auto mt-10 max-w-screen-lg'>
-        <ul className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-          {filteredSnippets.map((snippet) => (
-            <Snippet key={snippet.title} {...snippet} />
-          ))}
-        </ul>
-      </section>
-    </div>
+        <SelectType selected={typeSelected} setSelected={setTypeSelected} />
+
+        <section className='mx-auto mt-10 max-w-screen-lg'>
+          <ul className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {filteredSnippets.map((snippet) => (
+              <Snippet key={snippet.title} {...snippet} />
+            ))}
+          </ul>
+        </section>
+      </div>
+    </>
   );
 };
 
