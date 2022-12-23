4e32a0e4609cf92dccd2818cc33d2cbc67dc80d3
Alexis Guzman <codingcodax@gmail.com>
2022-12-22 20:57:31 -0600

    feat(app): add default seo component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 
 import Layout from '~/components/layout';
+
+import SEO from 'next-seo.config';
 
 import '~/styles/globals.css';
 
@@ -9,6 +12,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute='class' defaultTheme='system'>
       <Layout>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
