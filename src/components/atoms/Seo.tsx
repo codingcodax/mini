518e9afc9456d26d8f2e78e416dd11b0b3643203
Alexis Guzman <codingcodax@gmail.com>
2022-12-22 21:01:33 -0600

    feat(atoms): setup seo

--- src/components/atoms/Seo.tsx
+++ src/components/atoms/Seo.tsx
@@ -1,8 +1,35 @@
-const Seo = () => {
+import { NextSeo } from 'next-seo';
+
+import { description as defaultDescription, domain } from 'next-seo.config';
+
+interface SeoProps {
+  name: string;
+  path: string;
+  description?: string;
+  ogImage?: string;
+}
+
+const Seo = ({
+  name,
+  path,
+  description = defaultDescription,
+  ogImage = domain + '/og.png',
+}: SeoProps) => {
+  const title = `${name} | slug`;
+  const url = `${domain}${path}`;
+
   return (
-    <div>
-      <p>Seo component</p>
-    </div>
+    <NextSeo
+      canonical={url}
+      description={description}
+      openGraph={{
+        url,
+        title,
+        description,
+        images: [{ url: ogImage, alt: title }],
+      }}
+      title={title}
+    />
   );
 };
 
