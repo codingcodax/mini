6831f8488726b79b83c63ea663c086af9ea3fde8
Alexis Guzman <codingcodax@gmail.com>
2022-12-27 12:21:48 -0600

    fix(snippets): update input material sync transition

--- src/data/snippets/InputMaterial.tsx
+++ src/data/snippets/InputMaterial.tsx
@@ -5,7 +5,7 @@ const InputMaterial: Snippet = {
   type: 'input',
   githubLink:
     'https://github.com/codingcodax/mini/tree/main/src/data/snippets/InputMaterial.tsx',
-  playgroundLink: 'https://play.tailwindcss.com/Uv4YeMNIBI',
+  playgroundLink: 'https://play.tailwindcss.com/oksHNgOycs',
   source: (
     <div className='relative bg-slate-900 text-slate-50'>
       <input
@@ -16,7 +16,7 @@ const InputMaterial: Snippet = {
         type='text'
       />
       <label
-        className='absolute left-0 mx-1 cursor-text bg-inherit px-1 text-slate-500 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3 peer-focus:text-sm peer-focus:text-sky-400'
+        className='absolute left-0 -top-3 mx-1 cursor-text bg-inherit px-1 text-sm text-slate-500 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:transition-all peer-focus:-top-3 peer-focus:text-sm peer-focus:text-sky-400'
         htmlFor='name'
       >
         Name
