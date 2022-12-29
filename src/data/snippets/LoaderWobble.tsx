6f59829e70c3f26cda424e3f98a0cf0431d5cc02
Alexis Guzman <codingcodax@gmail.com>
2022-12-29 15:15:56 -0600

    fix(snippets): update size of loader wobble

--- src/data/snippets/LoaderWobble.tsx
+++ src/data/snippets/LoaderWobble.tsx
@@ -5,14 +5,14 @@ const LoaderWobble: Snippet = {
   type: 'loader',
   githubLink:
     'https://github.com/codingcodax/mini/tree/main/src/data/snippets/LoaderWobble.tsx',
-  playgroundLink: 'https://play.tailwindcss.com/WjB9P03al6',
+  playgroundLink: 'https://play.tailwindcss.com/Vk5Qy4pte2',
   source: (
-    <div className='relative flex w-10 items-center justify-start before:h-2 before:w-2 before:translate-x-full before:animate-[wobble_1s_ease-in-out_infinite] before:rounded-full before:bg-slate-50'></div>
+    <div className='relative flex w-14 items-center justify-start before:h-3 before:w-3 before:translate-x-full before:animate-[wobble_1s_ease-in-out_infinite] before:rounded-full before:bg-slate-50'></div>
   ),
   animation: `keyframes: {
         wobble: {
           '0%': { transform: 'translateX(0rem)' },
-          '50%': { transform: 'translateX(2rem)' },
+          '50%': { transform: 'translateX(2.7rem)' },
           '100%': { transform: 'translateX(0rem)' },
         },
       }`,
