7bc1c63269ddd5458e42b08f43da9c61f176e580
Alexis Guzman <codingcodax@gmail.com>
2022-12-29 15:23:30 -0600

    fix(snippets): update loader waveform size

--- src/data/snippets/LoaderWaveform.tsx
+++ src/data/snippets/LoaderWaveform.tsx
@@ -5,9 +5,9 @@ const LoaderWaveform: Snippet = {
   type: 'loader',
   githubLink:
     'https://github.com/codingcodax/mini/tree/main/src/data/snippets/LoaderWaveform.tsx',
-  playgroundLink: 'https://play.tailwindcss.com/8TM9j8oUgN',
+  playgroundLink: 'https://play.tailwindcss.com/6PpoviGw3q',
   source: (
-    <div className='flex h-8 w-9 flex-row flex-nowrap items-center justify-between'>
+    <div className='flex h-10 w-11 flex-row flex-nowrap items-center justify-between'>
       <div className='h-full w-1 animate-[grow_1s_ease-in-out_0.55s_infinite] bg-slate-50'></div>
       <div className='h-full w-1 animate-[grow_1s_ease-in-out_0.7s_infinite] bg-slate-50'></div>
       <div className='h-full w-1 animate-[grow_1s_ease-in-out_0.85s_infinite] bg-slate-50'></div>
