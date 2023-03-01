61ba5db78e2e06fd40ee7553bf4d6b520216c6d0
Alexis Guzman <codingcodax@gmail.com>
2023-03-01 08:50:20 -0600

    fix(snippets): update id and html for on input material two

--- src/data/snippets/InputMaterialTwo.tsx
+++ src/data/snippets/InputMaterialTwo.tsx
@@ -10,14 +10,14 @@ const InputMaterialTwo: Snippet = {
     <div className='relative text-slate-50'>
       <input
         className='peer h-10 w-36 border-b-2 border-slate-600 bg-transparent placeholder-transparent transition-colors duration-200 ease-in-out focus:border-sky-400 focus:outline-none'
-        id='name'
+        id='name-two'
         name='name'
         placeholder='Name'
         type='text'
       />
       <label
         className='absolute left-0 -top-3 cursor-text text-sm text-slate-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3 peer-focus:text-sm peer-focus:text-sky-400'
-        htmlFor='name'
+        htmlFor='name-two'
       >
         Name
       </label>
